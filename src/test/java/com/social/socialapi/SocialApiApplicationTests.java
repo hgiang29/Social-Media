246390fa55623a8d4bf9be92d42bb1199d9271b5
huy giang <huygiangt294@gmail.com>
2024-07-02 10:16:31 +0700

    add

---
+++ src/test/java/com/social/socialapi/SocialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social.socialapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
