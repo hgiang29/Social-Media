2b22f960926f770d83d554e5f8b76b33497c8df3
hieumai943 <hieunm.hrt@gmail.com>
2024-07-03 11:11:42 +0700

    DI for Post Service

--- src/main/java/com/social/socialapi/service/PostService.java
+++ src/main/java/com/social/socialapi/service/PostService.java
@@ -8,37 +8,11 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
-@Service
-public class PostService {
+public interface PostService {
 
-    @Autowired
-    public PostRepository postRepository;
-
-    public List<Post> getAllPosts(){
-        try {
-            return (List<Post>) postRepository.findAll();
-        } catch (Exception e) {
-            System.out.println("Error occurred while fetching posts: "+ e);
-            return new ArrayList<>();
-        }
-    }
-    public void addPost(Post post){
-        postRepository.save(post);
-    }
-    public Post getPostById(int id){
-        return postRepository.findById(id).orElse(new Post());
-    }
-    public void updatePost(Post post) {
-        postRepository.save(post);
-    }
-
-    public void deletePost(int postId) {
-        postRepository.deleteById(postId);
-
-    }
-
-//    public List<Post> search(String keyword) {
-//
-//        return postRepository.findByPostProfileContainingOrPostDescContaining(keyword,keyword);
-//    }
+    public List<Post> getAllPosts();
+    public void addPost(Post post);
+    public Post getPostById(int id) ;
+    public void updatePost(Post post);
+    public void deletePost(int postId);
 }
---
+++ src/main/java/com/social/socialapi/service/implement/PostServiceImpl.java
@@ -0,0 +1,44 @@
+package com.social.socialapi.service.implement;
+
+import com.social.socialapi.domain.entity.post.Post;
+import com.social.socialapi.repository.PostRepository;
+import com.social.socialapi.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PostServiceImpl implements PostService {
+    @Autowired
+    public PostRepository postRepository;
+
+    public List<Post> getAllPosts(){
+        try {
+            return (List<Post>) postRepository.findAll();
+        } catch (Exception e) {
+            System.out.println("Error occurred while fetching posts: "+ e);
+            return new ArrayList<>();
+        }
+    }
+    public void addPost(Post post){
+        postRepository.save(post);
+    }
+    public Post getPostById(int id){
+        return postRepository.findById(id).orElse(new Post());
+    }
+    public void updatePost(Post post) {
+        postRepository.save(post);
+    }
+
+    public void deletePost(int postId) {
+        postRepository.deleteById(postId);
+
+    }
+
+//    public List<Post> search(String keyword) {
+//
+//        return postRepository.findByPostProfileContainingOrPostDescContaining(keyword,keyword);
+//    }
+}
