ed33f1a2b8ca5bbdd8743cfbb157fd1a0b1acfc0
sh1nn0on3 <sanogaku31@gmail.com>
2024-07-04 09:42:45 +0700

    chore: Add HelloController and ApiResponse classes

--- src/main/java/com/social/socialapi/controller/HelloController.java
+++ src/main/java/com/social/socialapi/controller/HelloController.java
@@ -4,13 +4,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.social.socialapi.dto.outputdto.ApiResponse;
+
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public ResponseEntity<String> hello() {
+    public  ResponseEntity<String> hello() {
         return ResponseEntity.ok("hehe");
     }
 
+
+    @GetMapping("/hello2")
+    public ApiResponse<String> hello2() {
+        return ApiResponse.<String>builder().message("hehe").build();
+    }
 
 }
---
+++ src/main/java/com/social/socialapi/dto/outputdto/ApiResponse.java
@@ -0,0 +1,19 @@
+package com.social.socialapi.dto.outputdto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
+    @Builder.Default
+    private int code = 1000;
+    private String message;
+    private T result;
+}
