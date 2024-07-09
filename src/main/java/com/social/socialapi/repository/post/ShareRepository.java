a397ac1b012c86d22246509eb9c39ae59c407b94
hieumai943 <hieunm.hrt@gmail.com>
2024-07-09 22:47:47 +0700

    show share of user

--- src/main/java/com/social/socialapi/repository/post/ShareRepository.java
+++ src/main/java/com/social/socialapi/repository/post/ShareRepository.java
@@ -4,11 +4,17 @@ import com.social.socialapi.entity.post.Share;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface ShareRepository extends CrudRepository<Share, Integer> {
     @Modifying
     @Transactional
     @Query("DELETE FROM Share c WHERE c.post.id = :IdPost")
     void deleteByPostId(int IdPost);
+
+    @Query("select share from Share share where share.user.id = :userId")
+    List<Share> getShareByUserId(@Param("userId") int userId);
 }
