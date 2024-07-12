19639609ba4d9059abf7f86bb32cba2ec3724f48
huy giang <huygiangt294@gmail.com>
2024-07-12 18:31:10 +0700

    add message room order by latest message

--- src/main/java/com/social/socialapi/repository/message/RoomMessageRepository.java
+++ src/main/java/com/social/socialapi/repository/message/RoomMessageRepository.java
@@ -1,6 +1,7 @@
 package com.social.socialapi.repository.message;
 
 import com.social.socialapi.entity.message.RoomMessage;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -9,4 +10,7 @@ public interface RoomMessageRepository extends CrudRepository<RoomMessage, Integ
 
     RoomMessage findById(int id);
 
+    @Query(value = "SELECT rm.id FROM room_message rm JOIN messages m ON rm.id = m.room_message_id GROUP BY rm.id ORDER BY MAX(m.created_at) DESC",
+            nativeQuery = true)
+    List<Integer> getLatestRoomMessage();
 }
